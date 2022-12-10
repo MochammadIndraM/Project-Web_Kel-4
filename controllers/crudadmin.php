fb666a64ef0684da85ff5ea8fe2ed31916b626ad
Mochammad Indra M <Indrapagah43@gmail.com>
2022-12-10 20:46:22 +0700

    crud data admin

--- controllers/crudadmin.php
+++ controllers/crudadmin.php
@@ -2,88 +2,91 @@
 
 include '../koneksi.php';
 
-    if (isset($_POST['tambah'])) {
-        if($_POST['tnama'] == ""){
-            echo "<script>alert('Nama Harus diisi');
+if (isset($_POST['tambah'])) {
+    if ($_POST['tnama'] == "") {
+        echo "<script>alert('Nama Harus diisi');
                     document.location='../data-admin.php';
                 </script>";
-            } else  if($_POST['temail'] == ""){
-                echo "<script>alert('Email Harus diisi');
+    } else  if ($_POST['temail'] == "") {
+        echo "<script>alert('Email Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            } else  if($_POST['tpassword'] == ""){
-                echo "<script>alert('Password Harus diisi');
+    } else  if ($_POST['tpassword'] == "") {
+        echo "<script>alert('Password Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            } else  if($_POST['tnohp'] == ""){
-                echo "<script>alert('No HP Harus diisi');
+    } else  if ($_POST['tnohp'] == "") {
+        echo "<script>alert('No HP Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            }
-                
-       } else  {
-         
-        
+    } else {
         $tambah = mysqli_query($koneksi, "INSERT INTO user (id_user, username, email, password, no_hp, role)
-                                          VALUES ('null',
-                                                  '$_POST[tnama]',
-                                                  '$_POST[temail]',
-                                                  '$_POST[tpassword]',
-                                                  '$_POST[tnohp]',
-                                                  'admin')");
-    
-       
+                                                  VALUES ('null',
+                                                          '$_POST[tnama]',
+                                                          '$_POST[temail]',
+                                                          '$_POST[tpassword]',
+                                                          '$_POST[tnohp]',
+                                                          'admin')");
+
+
         if ($tambah) {
             echo "<script>alert('Berhasil Menambahkan Data');
-                    document.location='../data-admin.php';
-                </script>";
+                            document.location='../data-admin.php';
+                        </script>";
         } else {
             echo "<script>alert('Gagal Menambahkan Data');
-                    document.location='../data-admin.php';
-                </script>";
+                            document.location='../data-admin.php';
+                        </script>";
         }
-                                                
-    } 
-
-    if (isset($_POST['edit'])) {
-        if($_POST['tnama'] == ""){
-            echo "<script>alert('Nama Harus diisi');
+    }
+} else if (isset($_POST['edit'])) {
+    if ($_POST['tnama'] == "") {
+        echo "<script>alert('Nama Harus diisi');
                     document.location='../data-admin.php';
                 </script>";
-            } else  if($_POST['temail'] == ""){
-                echo "<script>alert('Email Harus diisi');
+    } else  if ($_POST['temail'] == "") {
+        echo "<script>alert('Email Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            } else  if($_POST['tpassword'] == ""){
-                echo "<script>alert('Password Harus diisi');
+    } else  if ($_POST['tpassword'] == "") {
+        echo "<script>alert('Password Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            } else  if($_POST['tnohp'] == ""){
-                echo "<script>alert('No HP Harus diisi');
+    } else  if ($_POST['tnohp'] == "") {
+        echo "<script>alert('No HP Harus diisi');
                         document.location='../data-admin.php';
                     </script>";
-            }
-                
-       } else  {
-         
-        
-        $edit = mysqli_query($koneksi, "UPDATE user SET username = '$_POST[tnama]'
-                                                        email    = '$_POST[temail]',
-                                                        password = '$_POST[tpassword]'
-                                                        no_hp    = '$_POST[tnohp]',
-                                                         WHERE id_user ='$_POST[tiduser]' ");
+    } else {
+        $edit = mysqli_query($koneksi, "UPDATE user SET username = '$_POST[tnama]', email = '$_POST[temail]', password = '$_POST[tpassword]', no_hp = '$_POST[tnohp]' WHERE id_user ='$_POST[tiduser]'");
     
-       
+    
         if ($edit) {
             echo "<script>alert('Berhasil Mengedit Data');
-                    document.location='../data-admin.php';
-                </script>";
+                        document.location='../data-admin.php';
+                    </script>";
         } else {
             echo "<script>alert('Gagal Mengedit Data');
-                    document.location='../data-admin.php';
-                </script>";
+                        document.location='../data-admin.php';
+                    </script>";
         }
-                                                
-    } 
+
+    }
+} else if (isset($_POST['hapus'])) {
+   
+        $hapus = mysqli_query($koneksi, "DELETE FROM user WHERE id_user ='$_POST[tiduser]'");
+    
+    
+        if ($hapus) {
+            echo "<script>alert('Berhasil Menghapus Data');
+                        document.location='../data-admin.php';
+                    </script>";
+        } else {
+            echo "<script>alert('Gagal Menghapus Data');
+                        document.location='../data-admin.php';
+                    </script>";
+        }
+
+    
+} 
 
 ?>
--- data-admin.php
+++ data-admin.php
@@ -253,14 +253,16 @@
 			<div class="container-fluid">
 
 				<!-- row -->
-				<button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#Modaltambahadmin">Tambah Admin</button>
+				
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="card">
 							<div class="card-header">
 								<h4 class="card-title">Data Admin</h4>
+								
 							</div>
 							<div class="card-body">
+							<button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#Modaltambahadmin">Tambah Admin</button>
 								<div class="table-responsive">
 									<table class="table table-responsive-md">
 										<thead>
@@ -295,8 +297,8 @@
 													<td scope="row"><?php echo $password ?></td>
 													<td scope="row"><?php echo $nohp ?></td>
 													<td scope="row">
-														<a href="#" class="btn btn-warning mb-2" data-bs-toggle="modal" data-bs-target="#Modaleditadmin">Edit</a>
-														<a href="#" class="btn btn-danger mb-2" data-bs-toggle="modal" data-bs-target="#Modalhapusadmin">Hapus</a>
+														<button type="button" class="btn btn-warning mb-2"  onclick="editmodal('<?= $id ?>', '<?= $nama ?>', '<?= $email ?>', '<?= $password ?>', '<?= $nohp ?>')">Edit</button>
+														<button type="button" class="btn btn-danger mb-2"  onclick="hapusmodal('<?= $id ?>')">Hapus</button>
 													</td>
 												</tr>
 
@@ -359,27 +361,26 @@
 							</div>
 							<div class="modal-body">
 								<form action="controllers/crudadmin.php" method="post">
-								<input type="text" class="form-control" value="<?= $id ?>"
-                                                    name="tiduser" readonly>
+									<input type="hidden" class="form-control" id="idadmin" name="tiduser">
 									<div class="mb-3 row">
 										<label class="form-label">Nama</label>
-										<input type="text" class="form-control" placeholder="Masukkan Nama" name="tnama" value="<?= $nama ?>" required>
+										<input type="text" class="form-control" id="namaadmin" placeholder="Masukkan Nama" name="tnama" >
 									</div>
 									<div class="mb-3 row">
 										<label class="form-label">Email</label>
-										<input type="text" class="form-control" placeholder="Masukkan Email" name="temail" value="<?= $email ?>" required>
+										<input type="text" class="form-control" placeholder="Masukkan Email" id="emailadmin" name="temail" >
 									</div>
 									<div class="mb-3 row">
 										<label class="form-label">Password</label>
-										<input type="text" class="form-control" placeholder=" Masukkan Password" name="tpassword"value="<?= $password ?>" required>
+										<input type="text" class="form-control" placeholder=" Masukkan Password" id="passwordadmin" name="tpassword" >
 									</div>
 									<div class="mb-3 row">
 										<label class="form-label">No HP</label>
-										<input type="text" class="form-control" placeholder="Masukkan No HP" name="tnohp"value="<?= $nohp ?>" required>
+										<input type="text" class="form-control" placeholder="Masukkan No HP" id="nohpadmin" name="tnohp">
 									</div>
 									<div class="modal-footer">
 										<button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
-										<button type="submit" class="btn btn-primary" name="tambah">Edit</button>
+										<button type="submit" class="btn btn-primary" name="edit">Edit</button>
 									</div>
 								</form>
 							</div>
@@ -387,6 +388,30 @@
 					</div>
 				</div>
 				<!-- Modal Edit End -->
+
+				<!-- Modal Hapus -->
+				<div class="modal fade" id="Modalhapusadmin" style="display: none;" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">Hapus Data Admin</h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal">
+								</button>
+							</div>
+							<div class="modal-body">
+								<form action="controllers/crudadmin.php" method="post">
+									<input type="hidden" class="form-control" id="idadminhapus" name="tiduser">
+									<h5 class="text-center"> Apakah Anda yakin akan menghapus data ini?</h5>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+										<button type="submit" class="btn btn-primary" name="hapus">Hapus</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Modal Hapus End -->
 
 			</div>
 		</div>
@@ -442,8 +467,27 @@
 	<script src="js/custom.min.js"></script>
 	<script src="js/dlabnav-init.js"></script>
 	<script src="js/demo.js"></script>
-	<script src="js/styleSwitcher.js"></script>
+	<!-- <script src="js/styleSwitcher.js"></script> -->
 	<script>
+		function editmodal(id, nama, email, password, nohp) {
+			// console.log(id + nama + email + password + nohp);
+
+			$('#idadmin').val(id);
+			$('#namaadmin').val(nama);
+			$('#emailadmin').val(email);
+			$('#passwordadmin').val(password);
+			$('#nohpadmin').val(nohp);
+
+			$('#Modaleditadmin').modal('show');
+		}
+
+		function hapusmodal(id) {
+			// console.log(id + nama + email + password + nohp);
+
+			$('#idadminhapus').val(id);
+
+			$('#Modalhapusadmin').modal('show');
+		}
 		function cardsCenter() {
 
 			/*  testimonial one function by = owl.carousel.js */
