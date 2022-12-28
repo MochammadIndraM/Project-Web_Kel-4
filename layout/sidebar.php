e6c6b4af96f19f5545b2c331497cfe00e9146af7
Mochammad Indra M <Indrapagah43@gmail.com>
2022-12-28 20:30:22 +0700

    Layout caller

---
+++ layout/sidebar.php
@@ -0,0 +1,43 @@
+		<!--**********************************
+            Sidebar start
+        ***********************************-->
+		<div class="dlabnav">
+			<div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+					<li><a href="index.php" aria-expanded="false">
+							<i class="fas fa-home"></i>
+							<span class="nav-text">Dashboard</span>
+						</a>
+
+					</li>
+
+					<li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-info-circle"></i>
+							<span class="nav-text">Master Data</span>
+						</a>
+						<ul aria-expanded="false">
+							<li><a href="data-admin.php">Data Admin</a></li>
+							<li><a href="data-customer.php">Data Customer</a></li>
+							<li><a href="data-daftar-harga-print.html">Daftar Harga Print</a></li>
+							<li><a href="data-barang.php">Data Barang</a></li>
+						</ul>
+					</li>
+					<li><a  href="transaksi-invoice.php" aria-expanded="false">
+							<i class="fas fa-file-alt"></i>
+							<span class="nav-text">Transaksi</span>
+						</a>
+					</li>
+					<li><a href="riwayat.php" aria-expanded="false">
+							<i class="fas fa-table"></i>
+							<span class="nav-text">Riwayat</span>
+						</a>
+					</li>
+					<div class="copyright">
+						<p><strong>PRON TEAM</strong> © 2022 All Rights Reserved</p>
+					</div>
+				</ul>
+			</div>
+		</div>
+		<!--**********************************
+            Sidebar end
+        ***********************************-->
